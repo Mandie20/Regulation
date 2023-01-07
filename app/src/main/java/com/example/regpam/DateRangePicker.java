dabe161889513bc7d9955ec2bb7479c22f1eca00
Mandie20 <jsamandina@gmail.com>
2023-01-07 14:20:04 -0500

    first setup login page

---
+++ app/src/main/java/com/example/regpam/DateRangePicker.java
@@ -0,0 +1,14 @@
+//package com.example.regpam;
+//
+//public class DateRangePicker {
+//    Calendar calendar = Calendar.getInstance();
+//    long currentTime = calendar.getTimeInMillis();
+//
+//    MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+//builder.setTitleText("Sélectionnez une plage de dates");
+//    MaterialDatePicker<Pair<Long, Long>> picker = builder.build();
+//
+//picker.show(getSupportFragmentManager(), picker.toString());
+//
+//
+//}
